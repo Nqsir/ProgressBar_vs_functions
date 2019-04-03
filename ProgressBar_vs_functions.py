b99f62d307d1e1468d47a3efe12a4b6108961f7f
Victor STEYAERT <40288838+Nqsir@users.noreply.github.com>
2019-04-03 09:10:30 +0200

    Update thanks to @nerahmou

--- ProgressBar_vs_functions.py
+++ ProgressBar_vs_functions.py
@@ -27,9 +27,10 @@ class Signals(QObject):
 
     Signals Class is called in each ProgressBarWorker() and FuncsWorker() creation
 
-    Our 3 signals give us the possibility to catch signals when starting progressing and finishing the run method, you
-    can choose what you want to be returned in my need (int) while progress none for the rest
+    Our 3 signals give us the possibility to catch signals when starting, progressing and finishing the run method, you
+    can choose what you want to be returned. In my case, I needed an int while progressing and none for the rest
     """
+    
     starting = pyqtSignal()
     progress = pyqtSignal(int)
     finish = pyqtSignal()
@@ -38,6 +39,7 @@ class Signals(QObject):
 class ProgressBarWorker(QThread):
     """
     Inherits from QThread to not freeze our soft => This is what this is about right ?
+    
     That class object is made too show updates with a defining time.sleep() to make it look smooth. The sleep duration
     goes with the self.time_limit in the above class FuncsWorker().
 
@@ -85,7 +87,7 @@ class FuncsWorker(QThread):
     The self.time_limit comes from : Each time a function is about to be executed, send a signal (self.signals.progress.emit(c))
     that will make progressbar grow, that growth takes approximatively (see in ProgressBarWorker time.sleep(0.1)) 0.1 sec * 100/nbr of functions
     => I want to go to 100% I've got 3 functions = 3.33 seconds and added and extra 1 sec to be sure, as a function can be faster executed,
-    I am checking the time it took to executed it (exec_time) and making it sleep until progressbar has reached its
+    I am checking the time it took to execute it (exec_time) and make it sleep until progressbar has reached its
     defined limit => self.time_limit.
     """
 
@@ -118,8 +120,7 @@ class FuncsWorker(QThread):
 class Actions(QDialog):
     """
     Simple dialog that consists of a Progress Bar and a Button.
-    Clicking on the button results in the start of a timer and
-    updates the progress bar.
+    Clicking on the button updates the progress bar.
     """
 
     def __init__(self):
@@ -152,7 +153,8 @@ class Actions(QDialog):
         self.funcs.start()
 
     # Creates a ProgressBarWorker class each time a function is about to be executed and defines the limits
-    # of the progression => first from 0 to 34, then 34 to 67 then ...
+    # of the progression. For 3 functions => first from 0 to 34, then 34 to 67 and finally 67 to 99 (99 because we still need
+    # to be sure everything is finished before starting again, see while count < 99 and count < self.args[0]: in ProgressBarWorker())
     # initialises progress signal of ProgressBarWorker to progressbar_update method
     def funcs_progress_return(self, value):
         self.graphic = ProgressBarWorker((100 / self.len_list_func) * (value + 1), (100 / self.len_list_func) * value)
